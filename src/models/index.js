ff7169ef6ae5d8088c52c953af923feb22fcd90a
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-02-07 18:51:50 -0300

    creation of index.js for optimization of model routes

---
+++ src/models/index.js
@@ -0,0 +1,9 @@
+const Category = require('./category.model');
+const Role = require('./role.model');
+const User = require('./user.model');
+
+module.exports = {
+    Category,
+    Role,
+    User
+}
