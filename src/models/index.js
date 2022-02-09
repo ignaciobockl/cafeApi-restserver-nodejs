4038b28754c79d0d7ca5f1253a22ded53a166cf4
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-02-08 22:04:55 -0300

    product model

--- src/models/index.js
+++ src/models/index.js
@@ -1,9 +1,11 @@
 const Category = require('./category.model');
 const Role = require('./role.model');
+const Product = require('./product.model');
 const User = require('./user.model');
 
 module.exports = {
     Category,
     Role,
+    Product,
     User
 }
---
+++ src/models/product.model.js
@@ -0,0 +1,18 @@
+const { Schema, model } = require('mongoose');
+
+const ProductSchema = Schema({
+    name: { type: String, require: [true, 'Name is required.'], unique: true },
+    state: { type: Boolean, default: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', require: true },
+    price: { type: Number, default: 0 },
+    category: { type: Schema.Types.ObjectId, ref: 'Category', require: true },
+    description: { type: String },
+    available: { type: Boolean, default: true }
+});
+
+ProductSchema.methods.toJSON = function() {
+    const { __v, state, ...product } = this.toObject();
+    return product;
+}
+
+module.exports = model('Product', ProductSchema);
