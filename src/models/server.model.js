84e66ad7772fd92a8f1c3ad0b7169882ccc03012
Ignacio Bockl <ignaciobockl96@gmail.com>
2022-02-08 22:03:45 -0300

    product route (path)

--- src/models/server.model.js
+++ src/models/server.model.js
@@ -18,6 +18,7 @@ class Server {
         this.paths = {
             auth: '/api/auth',
             categories: '/api/categories',
+            products: '/api/products',
             users: '/api/users'
         }
 
@@ -68,6 +69,7 @@ class Server {
 
         this.app.use(this.paths.auth, require('../routes/auth.routes'));
         this.app.use(this.paths.categories, require('../routes/categories.routes'));
+        this.app.use(this.paths.products, require('../routes/products.routes'));
         this.app.use(this.paths.users, require('../routes/users.routes'));
 
     }
